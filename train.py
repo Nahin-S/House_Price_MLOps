a0241982ef43bc3dc743c9328d5aa098321ba5c7
Demo <nahin12072025@gmail.com>
2026-07-17 22:16:06 +0530

    first setup for mlflow

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
-
+import mlflow
 
 housing = fetch_california_housing(as_frame = True)
 
@@ -9,10 +9,14 @@ X = housing.data
 y = housing.target
 
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2, random_state = 42)
+with mlflow.start_run():
+    model = RandomForestRegressor(
+        n_estimators = 20,
+        max_depth = 20
+    )
 
-model = RandomForestRegressor()
-
-model.fit(X_train, y_train)
-score = model.score(X_test,y_test)
-
-print(f'R2 score : {score :.4f}')
+    model.fit(X_train, y_train)
+    score = model.score(X_test,y_test)
+    mlflow.log_param("n_estimators", 100)
+    mlflow.log_metric("r2_score", score)
+    print(f'R2 score : {score :.4f}')
