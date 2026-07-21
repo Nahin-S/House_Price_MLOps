65e1f0bb3b320025a8678e49114da7b35a3f38aa
Demo <nahin12072025@gmail.com>
2026-07-21 08:56:18 +0530

    model with mlflow logging para and metric

--- train.py
+++ train.py
@@ -2,16 +2,16 @@ from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 import mlflow
-
+import mlflow.sklearn 
 housing = fetch_california_housing(as_frame = True)
 
 X = housing.data
 y = housing.target
-
+estimators = 30
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2, random_state = 42)
 with mlflow.start_run():
     model = RandomForestRegressor(
-        n_estimators = 20,
+        n_estimators = estimators,
         max_depth = 20
     )
 
@@ -19,4 +19,5 @@ with mlflow.start_run():
     score = model.score(X_test,y_test)
     mlflow.log_param("n_estimators", 100)
     mlflow.log_metric("r2_score", score)
+    mlflow.sklearn.log_model(model, "random_forest_model")
     print(f'R2 score : {score :.4f}')
